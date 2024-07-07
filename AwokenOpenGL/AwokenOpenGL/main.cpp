41227b58dce413eb364207c5644f6f44d1b214fc
Owen Knizak <138807889+AwokenOwen@users.noreply.github.com>
2024-07-06 21:17:26 -0400

    SHADERS YAY

--- AwokenOpenGL/AwokenOpenGL/main.cpp
+++ AwokenOpenGL/AwokenOpenGL/main.cpp
@@ -1,6 +1,8 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include <fstream>
+#include <string>
 
 //Function Protoypes
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
@@ -18,7 +20,7 @@ int main()
 	//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
 	//NOTE: Create window
-	GLFWwindow* window = glfwCreateWindow(800, 600, "Owen Knizak OpenGL Adventure", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(1600, 1200, "Owen Knizak OpenGL Adventure", NULL, NULL);
 	if (window == NULL)
 	{
 		std::cout << "Failed to create GLFW window" << std::endl;
@@ -35,24 +37,24 @@ int main()
 	}
 
 	//NOTE: Set OpenGL Viewport
-	glViewport(0, 0, 800, 600);
+	glViewport(0, 0, 1600, 1200);
 	
 	//NOTE: Change Viewport on screen change
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
 	//Generate vertices and store it in the buffer
 	float vertices[] = {
-		0.5f, 0.5f, 0.0f, // top right
-		0.5f, -0.5f, 0.0f, // bottom right
-		-0.5f, -0.5f, 0.0f, // bottom left
-		-0.5f, 0.5f, 0.0f // top left
-	};
-	unsigned int indices[] = { // note that we start from 0!
-		0, 1, 3, // first triangle
-		1, 2, 3 // second triangle
+		// positions // colors
+		0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, // bottom right
+		-0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, // bottom left
+		0.0f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f // top
 	};
 
+	unsigned int indices[] = {
+		0, 1, 2
+	};
 
+	//create shaders
 	unsigned int shaderProgram = *loadShaders(); 
 
 	unsigned int VAO = 0;
@@ -64,40 +66,55 @@ int main()
 	unsigned int EBO;
 	glGenBuffers(1, &EBO); 
 
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO); 
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, 
-		GL_STATIC_DRAW); 
-
 	// 1. bind Vertex Array Object
 	glBindVertexArray(VAO);
 	// 2. copy our vertices array in a buffer for OpenGL to use
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-	// 3. then set our vertex attributes pointers
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float),
+	// 3. copy our index array in a element buffer for OpenGL to use
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,
+		GL_STATIC_DRAW);
+	// 4. then set our vertex attributes pointers
+		//pos attribute
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float),
 		(void*)0);
 	glEnableVertexAttribArray(0);
+		//color attribute
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float),
+		(void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
 
 
-	
-
+	unsigned int frameCount = 0;
 	while (!glfwWindowShouldClose(window))
 	{
+
 		//input
 		processInput(window);
 
 		//rendering commands
-		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		//Activate Program
 		glUseProgram(shaderProgram);
+
+		//change color over time
+		float timeValue = glfwGetTime();
+		float greenValue = sin(timeValue) / 2.0f + 0.5f;
+		int vertexColorLocation = glGetUniformLocation(shaderProgram,
+			"ourColor");
+		glUniform4f(vertexColorLocation, 0.5f, greenValue, 0.75f, 1.0f);
+
 		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO); 
 		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 		//check and call events and swap buffers
 		glfwSwapBuffers(window);
 		glfwPollEvents();
+
+		frameCount++;
 	}
 
 	glfwTerminate();
@@ -120,12 +137,11 @@ void processInput(GLFWwindow* window)
 unsigned int* loadShaders()
 {
 	//Vertex Shader
-	const char* vertexShaderSource = "#version 330 core\n"
-		"layout (location = 0) in vec3 aPos;\n"
-		"void main()\n"
-		"{\n"
-		" gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-		"}\0";
+	std::ifstream vertShader("program.vert"); 
+	std::string vertContents((std::istreambuf_iterator<char>(vertShader)), 
+		std::istreambuf_iterator<char>());
+
+	const char* vertexShaderSource = vertContents.c_str();
 
 	//Compile Vertex Shader
 	unsigned int vertexShader;
@@ -146,12 +162,11 @@ unsigned int* loadShaders()
 	}
 
 	//Fragment Shader
-	const char* fragmentShaderSource = "#version 330 core\n"
-		"out vec4 FragColor;\n"
-		"void main()\n"
-		"{\n"
-		" FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-		"}\0";
+	std::ifstream fragShader("program.frag");
+	std::string fragContents((std::istreambuf_iterator<char>(fragShader)),
+		std::istreambuf_iterator<char>());
+
+	const char* fragmentShaderSource = fragContents.c_str();
 
 	//Compile Fragment Shader
 	unsigned int fragmentShader;
